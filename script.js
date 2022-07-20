08a792ba32abace7b461ecb9587b94044cef3327
ZippyDaFish <andrew.riker21@gmail.com>
2022-07-19 21:40:49 -0400

    Basic framework for sketching grid

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function generateGrid(){
-    const newDiv = document.createElement("div");
-    document.getElementById("container").appendChild(newDiv);
-}
+const container = document.getElementById("container");
 
-generateGrid();
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
