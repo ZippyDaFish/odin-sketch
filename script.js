46459085ca551aa8310e27e35b7147f4c9ec2e89
ZippyDaFish <andrew.riker21@gmail.com>
2022-07-20 20:59:00 -0400

    Add div coloring on hover

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
+    cell.addEventListener('mouseover', function handleMouseOver(){
+      cell.style.backgroundColor = 'blue';
+    })
     container.appendChild(cell).className = "grid-item";
   };
 };
